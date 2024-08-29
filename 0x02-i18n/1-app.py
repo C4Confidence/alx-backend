2c526950d4981beac79313bdd64ab5293d168c57
C4Confidence <iamconfidence5@gmail.com>
2024-08-29 16:59:13 +0100

    Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+"""Basic Babel Setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+# Config class with language and timezone settings
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Apply the configuration to the app
+app.config.from_object(Config)
+
+# Instantiate Babel
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Handles / Route"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
