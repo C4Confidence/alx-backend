37f246b9126ee8edf7904035287e69eb8e31f6af
C4Confidence <iamconfidence5@gmail.com>
2024-08-29 17:44:34 +0100

    Get Locale

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 
-"""Basic Babel Setup"""
+"""Get Locale from request"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babelex import Babel
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 # Config class with language and timezone settings
 class Config:
-    """Configuration for Babel"""
+    """Language Configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,14 +20,21 @@ class Config:
 # Apply the configuration to the app
 app.config.from_object(Config)
 
+
 # Instantiate Babel
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """Handles / Route"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
